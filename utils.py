bed1a6a743e237da72ee69496871e78081f93b4f
Camilo Fosco <camilofosco@gmail.com>
2023-09-15 00:26:29 -0400

    utils update

--- utils.py
+++ utils.py
@@ -3,6 +3,14 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import os
 import pickle as pkl
+import cv2
+from himalaya.scoring import correlation_score
+from PIL import Image
+import torch
+
+from moviepy.editor import VideoFileClip
+from moviepy.video.fx.all import speedx
+# from pygifsicle import optimize
 
 
 
@@ -23,6 +31,7 @@ def load_all_fmri_for_subject(path_to_subject_data: str) -> dict:
             fmri_data[brain_region] = data
 
     return fmri_data
+
 
 def compute_tsne_embeddings(data: dict, perplexity: int = 50, n_iter: int = 3000, average_over: str = 'repetitions') -> np.ndarray:
     """
@@ -60,10 +69,6 @@ def t_sne_visualization(data: dict, perplexity: int = 50, n_iter: int = 3000, av
 
 
 def transform_vids_to_gifs(path_to_vids='data/stimuli/mp4', path_to_gifs='data/stimuli/gif', size=128):
-    from moviepy.editor import VideoFileClip
-    from moviepy.video.fx.all import speedx
-    from pygifsicle import optimize
-    from PIL import Image
 
     os.makedirs(path_to_gifs, exist_ok=True)
 
@@ -75,28 +80,193 @@ def transform_vids_to_gifs(path_to_vids='data/stimuli/mp4', path_to_gifs='data/s
 
     # Process each video file
     for video_file in sorted(video_files):
-        print(f'Processing {video_file}...')
-
-        # Load the video
-        video_clip = VideoFileClip(os.path.join(path_to_vids, video_file))
-
-        # Speed up clip (reduces frames)
-        video_clip = speedx(video_clip, factor=4)
-
-        # Reduce resolution
-        video_clip = video_clip.resize(height=size)
-        
-        # Get the video file name without extension
-        video_name = os.path.splitext(video_file)[0]
-
-        # Create a gif file path
-        gif_file_path = os.path.join(path_to_gifs, f"{video_name}.gif")
-
-        # Convert to gif and save
-        video_clip.write_gif(gif_file_path, program='ffmpeg', opt='optimizeplus', fuzz=5)
-
-        # Optimize the gif file
-        # optimize(gif_file_path)
-
+        vid_to_gif(os.path.join(path_to_vids, video_file), os.path.join(path_to_gifs, video_file.replace('mp4','.gif')), size=size)
 
     print('All videos are converted to GIFs.')
+
+
+def frames_to_vid(frames, output_video_path, fps=30):
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    h, w, c = frames[0].shape
+    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps=fps, frameSize=(w, h))
+    for i in range(len(frames)):
+        img = cv2.cvtColor(frames[i], cv2.COLOR_RGB2BGR)
+        video_writer.write(img)
+    
+    video_writer.release()
+
+def vid_to_gif(video_filepath, output_gif_filepath, size=256):
+    
+    # print(f'Processing {video_filepath}...')
+
+    # Load the video
+    video_clip = VideoFileClip(video_filepath)
+
+    # Speed up clip (reduces frames)
+    video_clip = speedx(video_clip, factor=4)
+
+    # Reduce resolution
+    video_clip = video_clip.resize(height=size)
+
+    # Convert to gif and save
+    video_clip.write_gif(output_gif_filepath, 
+                         program='ffmpeg', 
+                         opt='optimizeplus', 
+                         fuzz=5,
+                         verbose=False)
+
+    # Optimize the gif file
+    # optimize(gif_file_path)
+
+
+def prompt_list_from_boldmoments_annots(annots):
+    # for i, a in annots.items():
+    #         for c in range(len(a['text_descriptions'])):
+    #             prompt = a['text_descriptions'][c]
+    pass
+
+def compute_metrics(targets, preds, verbose=True):
+
+    # Compute MSE
+    mse = np.mean((targets - preds)**2)
+
+    # Compute MAE
+    mae = np.mean(np.abs(targets - preds))
+
+    # Compute r2
+    r2 = 1 - np.sum((targets - preds)**2) / np.sum((targets - np.mean(targets))**2)
+
+    # Compute correlation score
+    corr = correlation_score(targets.T,preds.T).mean().item()
+
+    if verbose:
+        print(f'MSE: {mse}')
+        print(f'MAE: {mae}')
+        print(f'R2: {r2}')
+        print(f'Correlation: {corr}')
+
+    return {'mse': mse, 'mae': mae, 'r2': r2, 'corr': corr}
+
+
+def load_frames_to_tensor(root_dir, batch_size=8, n_frames_to_load=45, size=268):
+    """
+    Load image frames from subfolders into a torch tensor.
+    
+    Args:
+    - root_dir (str): Path to the root directory containing subfolders of frames.
+
+    Returns:
+    - Tensor of shape (b, f, c, h, w) where:
+      b: number of videos (subfolders)
+      f: number of frames per video (45 for bold moments)
+      c: number of channels (3 for RGB images)
+      h: height of the video
+      w: width of the video
+    """
+    
+    # List all subfolders (videos)
+    video_folders = [d for d in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, d))]
+    
+    # Sort the folders for consistency
+    video_folders.sort()
+    
+    # Placeholder list to store all video tensors
+    all_videos = []
+    
+    for video_folder in video_folders:
+        video_path = os.path.join(root_dir, video_folder)
+        
+        # List all frames in the current video folder
+        frames = [f for f in os.listdir(video_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
+        
+        # Sort the frames for consistency
+        frames.sort()
+        
+        # Placeholder list to store all frame tensors for the current video
+        video_frames = []
+        
+        for frame in frames:
+            frame_path = os.path.join(video_path, frame)
+            
+            # Load the image using PIL and convert to RGB
+            img = Image.open(frame_path).convert('RGB')
+            
+            # Convert the PIL image to a torch tensor
+            tensor = torch.tensor(np.array(img)).permute(2, 0, 1).float() / 255.0
+            
+            video_frames.append(tensor)
+        
+        # Stack all frame tensors for the current video
+        video_tensor = torch.stack(video_frames, dim=0)
+        all_videos.append(video_tensor)
+    
+    # Stack all video tensors
+    result_tensor = torch.stack(all_videos, dim=0)
+    
+    return result_tensor
+
+
+
+def preprocess_video_for_vid2vid_pipeline(video):
+    supported_formats = (np.ndarray, torch.Tensor, PIL.Image.Image)
+
+    if isinstance(video, supported_formats):
+        video = [video]
+    elif not (isinstance(video, list) and all(isinstance(i, supported_formats) for i in video)):
+        raise ValueError(
+            f"Input is in incorrect format: {[type(i) for i in video]}. Currently, we only support {', '.join(supported_formats)}"
+        )
+
+    if isinstance(video[0], PIL.Image.Image):
+        video = [np.array(frame) for frame in video]
+
+    if isinstance(video[0], np.ndarray):
+        video = np.concatenate(video, axis=0) if video[0].ndim == 5 else np.stack(video, axis=0)
+
+        if video.dtype == np.uint8:
+            video = np.array(video).astype(np.float32) / 255.0
+
+        if video.ndim == 4:
+            video = video[None, ...]
+
+        video = torch.from_numpy(video.transpose(0, 4, 1, 2, 3))
+
+    elif isinstance(video[0], torch.Tensor):
+        video = torch.cat(video, axis=0) if video[0].ndim == 5 else torch.stack(video, axis=0)
+
+        # don't need any preprocess if the video is latents
+        channel = video.shape[1]
+        if channel == 4:
+            return video
+
+        # move channels before num_frames
+        video = video.permute(0, 2, 1, 3, 4)
+
+    # normalize video
+    video = 2.0 * video - 1.0
+
+    return video
+
+
+def save_vectors_npy(vectors, save_path, video_names):
+    os.makedirs(save_path, exist_ok=True)
+
+    if type(vectors) == torch.Tensor:
+        ran = range(vectors.shape[0])
+    elif type(vectors) == list:
+        ran = range(len(vectors))
+
+    # Save each target vector for each video as its own npy file
+    for i in ran:
+        np.save(os.path.join(save_path, f'{video_names[i]}.npy'), vectors[i])
+
+
+
+def print_current_gpu_memory():
+    t = torch.cuda.get_device_properties(0).total_memory / 1024**2
+    r = torch.cuda.memory_reserved(0) / 1024**2
+    a = torch.cuda.memory_allocated(0) / 1024**2
+    f = r-a  # free inside reserved
+
+    print(f"GPU Mem -- Total: {int(t)} MB, Reserved: {int(r)}, Allocated: {int(a)}, Free: {int(f)}")
